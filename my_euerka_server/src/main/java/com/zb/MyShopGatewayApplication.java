6fa998a6841b95fa8573b9dee81cdadfd40159e9
wangxiao <2244141460@qq.com>
2019-09-03 14:07:30 +0800

    local update file

--- my_euerka_server/src/main/java/com/zb/MyShopGatewayApplication.java
+++ my_euerka_server/src/main/java/com/zb/MyShopGatewayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-//本地新开发功能1
+//本地新开发功能
 //远程修改了代码
 @SpringBootApplication
 @EnableZuulProxy
